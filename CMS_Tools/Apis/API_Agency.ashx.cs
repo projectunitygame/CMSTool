3c2e43989887939d480707ffc043b20f8d631887
dien-nguyen <truongdien99@gmail.com>
2019-07-09 09:14:13 +0700

    up123

--- CMS_Tools/Apis/API_Agency.ashx.cs
+++ CMS_Tools/Apis/API_Agency.ashx.cs
@@ -5,13 +5,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.SessionState;
 
 namespace CMS_Tools.Apis
 {
     /// <summary>
     /// Summary description for API_Agency
     /// </summary>
-    public class API_Agency : IHttpHandler
+    public class API_Agency : IHttpHandler, IRequiresSessionState
     {
         UserInfo accountInfo;
         Result result = new Result();
@@ -39,6 +40,12 @@ namespace CMS_Tools.Apis
                     case Constants.REQUEST_AGENCY_TYPE.CREATE_AGENCY:
                         CREATE_AGENCY(context);
                         break;
+                    case Constants.REQUEST_AGENCY_TYPE.LOCK_AGENCY:
+                        LOCK_AGENCY(context);
+                        break;
+                    case Constants.REQUEST_AGENCY_TYPE.UNLOCK_AGENCY:
+                        UNLOCK_AGENCY(context);
+                        break;
                     default:
                         break;
                 }
@@ -51,6 +58,122 @@ namespace CMS_Tools.Apis
                 context.Response.Write(JsonConvert.SerializeObject(result));
             }
         }
+        /// <summary>
+        /// Mở khóa đại lý
+        /// </summary>
+        /// <param name="context"></param>
+        private void UNLOCK_AGENCY(HttpContext context)
+        {
+            try
+            {
+                if (context.Session["UNLOCK_AGENCY"] == null || ((DateTime)context.Session["UNLOCK_AGENCY"] - DateTime.Now).TotalMilliseconds < Constants.TIME_REQUEST)
+                {
+                    string json = context.Request.Form["json"];
+                    if (!string.IsNullOrEmpty(json))
+                    {
+                        var jsonData = JsonConvert.DeserializeObject<UnLockAgencyEntity>(json);
+                        if (jsonData != null)
+                        {
+                            Logs.SaveLog(JsonConvert.SerializeObject(jsonData));
+                            if (string.IsNullOrEmpty(jsonData.agencyID))
+                            {
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
+                                result.msg = "Mã đại lý không được trống!";
+                            }
+                            else
+                            {
+                                jsonData.creatorID = accountInfo.AccountId;
+                                jsonData.creatorName = accountInfo.UserName;
+
+                                PayloadApi p = new PayloadApi()
+                                {
+                                    clientIP = UtilClass.GetIPAddress(),
+                                    data = Encryptor.EncryptString(JsonConvert.SerializeObject(jsonData), Constants.API_SECRETKEY)
+                                };
+                                var responseData = UtilClass.SendPost(JsonConvert.SerializeObject(p), Constants.API_URL + "api/v1/Agency/UnLockAgency");
+                                context.Response.Write(responseData);
+                                return;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    result.status = Constants.NUMBER_CODE.ERROR_CONNECT_SERVER;
+                    result.msg = Constants.NUMBER_CODE.ERROR_CONNECT_SERVER.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logs.SaveError("ERROR UNLOCK_AGENCY: " + ex);
+                result.status = Constants.NUMBER_CODE.ERROR_EX;
+                result.msg = Constants.NUMBER_CODE.ERROR_EX.ToString();
+            }
+            finally
+            {
+                context.Session["UNLOCK_AGENCY"] = DateTime.Now;
+            }
+            context.Response.Write(JsonConvert.SerializeObject(result));
+        }
+
+        /// <summary>
+        /// Khóa đại lý
+        /// </summary>
+        /// <param name="context"></param>
+        private void LOCK_AGENCY(HttpContext context)
+        {
+            try
+            {
+                if (context.Session["LOCK_AGENCY"] == null || ((DateTime)context.Session["LOCK_AGENCY"] - DateTime.Now).TotalMilliseconds < Constants.TIME_REQUEST)
+                {
+                    string json = context.Request.Form["json"];
+                    if (!string.IsNullOrEmpty(json))
+                    {
+                        var jsonData = JsonConvert.DeserializeObject<LockAgencyEntity>(json);
+                        if (jsonData != null)
+                        {
+                            Logs.SaveLog(JsonConvert.SerializeObject(jsonData));
+                            if (string.IsNullOrEmpty(jsonData.agencyID))
+                            {
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
+                                result.msg = "Mã đại lý không được trống!";
+                            }
+                            else
+                            {
+                                jsonData.creatorID = accountInfo.AccountId;
+                                jsonData.creatorName = accountInfo.UserName;
+
+                                PayloadApi p = new PayloadApi()
+                                {
+                                    clientIP = UtilClass.GetIPAddress(),
+                                    data = Encryptor.EncryptString(JsonConvert.SerializeObject(jsonData), Constants.API_SECRETKEY)
+                                };
+                                var responseData = UtilClass.SendPost(JsonConvert.SerializeObject(p), Constants.API_URL + "api/v1/Agency/LockAgency");
+                                context.Response.Write(responseData);
+                                return;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    result.status = Constants.NUMBER_CODE.ERROR_CONNECT_SERVER;
+                    result.msg = Constants.NUMBER_CODE.ERROR_CONNECT_SERVER.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logs.SaveError("ERROR LOCK_AGENCY: " + ex);
+                result.status = Constants.NUMBER_CODE.ERROR_EX;
+                result.msg = Constants.NUMBER_CODE.ERROR_EX.ToString();
+            }
+            finally
+            {
+                context.Session["LOCK_AGENCY"] = DateTime.Now;
+            }
+            context.Response.Write(JsonConvert.SerializeObject(result));
+        }
+
 
         /// <summary>
         /// Tạo đại lý cấp 1 mới
@@ -58,27 +181,83 @@ namespace CMS_Tools.Apis
         /// <param name="context"></param>
         private void CREATE_AGENCY(HttpContext context)
         {
+            short debug = 0;
             try
             {
                 if (context.Session["CREATE_AGENCY"] == null || ((DateTime)context.Session["CREATE_AGENCY"] - DateTime.Now).TotalMilliseconds < Constants.TIME_REQUEST)
                 {
+                    debug = 1;
                     string json = context.Request.Form["json"];
                     if (!string.IsNullOrEmpty(json))
                     {
+                        debug = 2;
                         var jsonData = JsonConvert.DeserializeObject<AgencyEntity>(json);
-                        if(jsonData != null)
+                        if (jsonData != null)
                         {
+                            debug = 3;
                             if (string.IsNullOrEmpty(jsonData.agencyCode))
                             {
-                                result.status = Constants.NUMBER_CODE.DATA_NULL;
+                                debug = 301;
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
                                 result.msg = "Mã đại lý không được bỏ trống!";
+                            }
+                            else if(jsonData.agencyCode.Length < 6 || jsonData.agencyCode.Length >20)
+                            {
+                                debug = 302;
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
+                                result.msg = "Mã đại lý phải từ 6-20 ký tự";
                             }
                             else if (string.IsNullOrEmpty(jsonData.password))
                             {
-                                result.status = Constants.NUMBER_CODE.DATA_NULL;
-                                result.msg = "!";
+                                debug = 303;
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
+                                result.msg = "Password không được để trống";
+                            }
+                            else if (jsonData.password.Length <6 && jsonData.password.Length >20)
+                            {
+                                debug = 304;
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
+                                result.msg = "Password phải từ 6-20 ký tự";
+                            }
+                            else if (jsonData.email.Length > 80)
+                            {
+                                debug = 3051;
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
+                                result.msg = "Email không được dài hơn 80 ký tự";
+                            }
+                            else if (string.IsNullOrEmpty(jsonData.phone))
+                            {
+                                debug = 306;
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
+                                result.msg = "Số điện thoại không được để trống";
+                            }
+                            else if (jsonData.phone.Length != 10)
+                            {
+                                debug = 307;
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
+                                result.msg = "Số điện thoại phải là 10 chữ số";
+                            }
+                            else if (string.IsNullOrEmpty(jsonData.displayName))
+                            {
+                                debug = 308;
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
+                                result.msg = "Tên hiển thị không được để trống";
+                            }
+                            else if (jsonData.displayName.Length < 6)
+                            {
+                                debug = 309;
+                                result.status = Constants.NUMBER_CODE.INFO_CREATE_AGENCY_VALI;
+                                result.msg = "Tên hiển thị phải nhiều hơn 5 ký tự";
                             }
                             else {
+                                debug = 310;
+
+                                jsonData.IP = UtilClass.GetIPAddress();
+                                jsonData.creatorID = accountInfo.AccountId;
+                                jsonData.creatorName = accountInfo.UserName;
+                                jsonData.limitTransaction = Constants.limitTransaction;
+                                jsonData.limitTransactionDaily = Constants.limitTransactionDaily;
+
                                 PayloadApi p = new PayloadApi()
                                 {
                                     clientIP = UtilClass.GetIPAddress(),
@@ -91,30 +270,34 @@ namespace CMS_Tools.Apis
                         }
                         else
                         {
+                            debug = 4;
                             result.status = Constants.NUMBER_CODE.DATA_NULL;
                             result.msg = Constants.NUMBER_CODE.DATA_NULL.ToString();
                         }
                     }
                     else
                     {
+                        debug = 5;
                         result.status = Constants.NUMBER_CODE.DATA_NULL;
                         result.msg = Constants.NUMBER_CODE.DATA_NULL.ToString();
                     }
                 }
                 else
                 {
+                    debug = 6;
                     result.status = Constants.NUMBER_CODE.ERROR_CONNECT_SERVER;
                     result.msg = Constants.NUMBER_CODE.ERROR_CONNECT_SERVER.ToString();
                 }
             }
             catch (Exception ex)
             {
-                Logs.SaveError("ERROR CREATE_AGENCY: " + ex);
+                Logs.SaveError("ERROR CREATE_AGENCY: [debug]:" +debug + "\n,\n" + ex);
                 result.status = Constants.NUMBER_CODE.ERROR_EX;
                 result.msg = Constants.NUMBER_CODE.ERROR_EX.ToString();
             }
             finally
             {
+                Logs.SaveError("[debug]:" + debug);
                 context.Session["CREATE_AGENCY"] = DateTime.Now;
             }
             context.Response.Write(JsonConvert.SerializeObject(result));
@@ -129,6 +312,9 @@ namespace CMS_Tools.Apis
         }
     }
 
+
+
+    #region EnityClass
     public class AgencyEntity
     {
         public string agencyCode;
@@ -143,4 +329,22 @@ namespace CMS_Tools.Apis
         public int creatorID;
         public string creatorName;
     }
+
+    public class LockAgencyEntity
+    {
+        public string agencyID;
+        public string note; //ghi chú khi khoa tai khoan của đại lý
+        public int creatorID;
+        public string creatorName;
+    }
+
+    public class UnLockAgencyEntity
+    {
+        public string agencyID;
+        public int creatorID;
+        public string creatorName;
+    }
+    #endregion
+
+
 }
